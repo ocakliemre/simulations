1da20af9b13f908fb9905b7a73019f58175c0aa9
l0x6c <l0x6c@yandex.com>
2018-11-23 17:28:06 +0300

    projectile motion

--- projectile-motion/python/simulation.py
+++ projectile-motion/python/simulation.py
@@ -3,8 +3,9 @@
 
 __author__ = 'l0x6c'
 
-import sys, math
-import time, numpy
+import sys
+import math
+import time
 
 import pygame
 import pygame.freetype
@@ -21,13 +22,21 @@ max_range   = V_x * flight_time     = 38.4
 '''
 
 class Object(object):
-    def __init__(self, a, v, g):
+    def __init__(self, a, v, g, p=60):
+        self.p = p
         self.g = g
         self.a = math.radians(a)
         self.v = list(map(
             lambda x: round(x * v), 
             [math.cos(self.a), math.sin(self.a)]
         ))
+
+        self.pos = (0, 0)
+        self.passed_t = 0
+
+        self._time = 2 * self.v[1] / g
+        self.range = self.v[0] * self._time
+        self.max_y = pow(self.v[1], 2) / (g * 2)
 
     def __str__(self):
         return 'Object(angle={}, velocity={}, gravity={})'.format(
@@ -45,56 +54,47 @@ class Object(object):
             self.v[1] * t - self.g * pow(t, 2) / 2
         )
 
+    def update(self):
+        if self.passed_t >= self._time:
+            self.passed_t = 0
+
+        self.passed_t += self._time / self.p
+        self.pos = self.position(self.passed_t)
+
 class Simulation(object):
     def __init__(self, size, a, v, g=10):
         self.size = size
-        
         self.font = pygame.freetype.Font('/usr/share/fonts/TTF/Hack-Regular.ttf', 12)
         self.screen = pygame.display.set_mode(size)
         self.screen.fill([0] * 3)
 
         self.object = Object(a, v, g)
-        self.values = list(map(
-            lambda t: self.object.position(t),
-            numpy.linspace(0, 2 * self.object.v[1] / g)
-        ))
-
-    def __call__(self):
-        while True:
-            frame = []
-
-            for x, y in self.values:
-                frame.append((float(x) * 5, -float(y) * 5))
-                yield frame
 
     def render_text(self, text, pos, color=(255, 255, 255)):
         self.font.render_to(self.screen, pos, text, color)
 
-    def main(self):
-        max_x = self.values[-1][0]
-        max_y = sorted(self.values, key=lambda x: x[1])[-1][1]
-        
-        step_x = 0 if max_x == 0 else self.size[0] / max_x
-        step_y = self.size[1] / max_y
+    def map_(self, num, a, b, c, d):
+        return int((d - c) * (num - a) / (b - a) + c)
 
-        values = [
-            (x * step_x, self.size[1] - y * step_y)
-            for x, y in self.values
-        ]
-        
-        for index in range(2, len(values) + 1):
+    def main(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit(); sys.exit()
+
             self.screen.fill([0] * 3)
 
-            x, y = values[:index][-1]
-            x, y = 0 if x == 0 else x / step_x, (self.size[1] - y) / step_y
+            x, y = self.object.pos
+            x_ = self.map_(x, 0, self.object.range, 0, self.size[0])
+            y_ = self.map_(y, 0, self.object.max_y, self.size[1], 0)
             
             self.render_text(str(self.object), (6, 6))
-            self.render_text('range: {}'.format(max_x), (6, 20))
-            self.render_text('max_y: {}'.format(max_y), (6, 34))
-            self.render_text('curr pos: [{}, {}]'.format(x, y), (6, 48))
+            self.render_text('range: {}'.format(self.object.range), (6, 20))
+            self.render_text('max_y: {}'.format(self.object.max_y), (6, 34))
+            self.render_text('curr pos: [{}, {}]'.format(*map(round, (x, y))), (6, 48))
 
-            pygame.draw.lines(self.screen, [255] * 3, False, values[:index])
-            pygame.display.update()
+            pygame.draw.circle(self.screen, [255] * 3, (x_, y_), 5, 1)
+            pygame.display.update(); self.object.update()
             
             time.sleep(0.09)
 
